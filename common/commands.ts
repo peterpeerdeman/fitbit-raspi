c70a04b6a06b1aa6d681d6780353560f9494674e
Peter Peerdeman <peter@lifely.nl>
2020-01-18 18:13:19 +0100

    feat(commands): define broker commands

---
+++ common/commands.ts
@@ -0,0 +1,7 @@
+// To FitBit
+export const UPDATE_UI = 'UPDATE_UI';
+
+// To Companion
+export const LIGHTS_UPDATE_GROUPLIST = 'LIGHTS_UPDATE_GROUPLIST';
+export const LIGHTS_GROUP_ON = 'LIGHTS_GROUP_ON';
+export const LIGHTS_GROUP_OFF = 'LIGHTS_GROUP_OFF';
