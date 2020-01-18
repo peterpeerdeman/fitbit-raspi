426b7312bf242b07af2172cea9eb9e1e0d201134
Peter Peerdeman <peter@lifely.nl>
2020-01-18 18:12:09 +0100

    feat(types): add and configure data types

--- common/types.ts
+++ common/types.ts
@@ -1,1 +1,16 @@
 // Add types used throughout your project here.
+
+declare namespace Raspi {
+    interface LightCard {
+        title: string,
+        subtitle: string,
+        groupId: number
+    }
+
+    interface State {
+        page: string,
+        lights: {
+            groupsData: Array<LightCard>
+        }
+    }
+}
