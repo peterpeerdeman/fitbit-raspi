365a9020e349d571a296a81d4f96b0c7dde876ae
Peter Peerdeman <peterpeerdeman@gmail.com>
2021-07-25 22:44:40 +0200

    fix(lights): fix lights with new v3 hue api data

--- companion/components/lights.ts
+++ companion/components/lights.ts
@@ -1,43 +1,54 @@
-import * as Commands from "../../common/commands";
+import * as Commands from '../../common/commands';
 import { App } from '../app';
-import { LightsApi } from '../apis/LightsApi'
+import { LightsApi } from '../apis/LightsApi';
 
 export class Lights {
-  app: App;
-  api: LightsApi
+    app: App;
+    api: LightsApi;
 
-  constructor(app: App) {
-    this.app = app;
-    this.api = new LightsApi();
-  }
+    constructor(app: App) {
+        this.app = app;
+        this.api = new LightsApi();
+    }
 
-  initialize() {
-    this.app.broker.registerHandler(Commands.LIGHTS_UPDATE_GROUPLIST, async () => {
-      console.log('companion command UPDATE_LIGHTS received');
-      this.app.resetState();
-      this.app.state.page = 'lights';
-      const groups = await this.api.getGroups();
-      this.app.state.lights.groupsData = groups.map(function (group) {
-        return {
-          title: group._rawData.name,
-          subtitle: 'all: ' + group._rawData.state.all_on + ', any: ' + group._rawData.state.any_on,
-          _id: group._id,
-        };
-      });
+    initialize() {
+        this.app.broker.registerHandler(
+            Commands.LIGHTS_UPDATE_GROUPLIST,
+            async () => {
+                console.log('companion command UPDATE_LIGHTS received');
+                this.app.resetState();
+                this.app.state.page = 'lights';
+                const groups = await this.api.getGroups();
+                this.app.state.lights.groupsData = groups.map(function (group) {
+                    return {
+                        title: group._data.name,
+                        subtitle:
+                            'all: ' +
+                            group._data.state.all_on +
+                            ', any: ' +
+                            group._data.state.any_on,
+                        _id: group._data.id,
+                    };
+                });
 
-      this.app.updateUi();
-    });
+                this.app.updateUi();
+            }
+        );
 
-    this.app.broker.registerHandler(Commands.LIGHTS_GROUP_ON, async (_id) => {
-      this.api.lightsOn(_id);      
-    });
+        this.app.broker.registerHandler(
+            Commands.LIGHTS_GROUP_ON,
+            async (_id) => {
+                this.api.lightsOn(_id);
+            }
+        );
 
-    this.app.broker.registerHandler(Commands.LIGHTS_GROUP_OFF, async (_id) => {
-      this.api.lightsOff(_id);      
-    });
+        this.app.broker.registerHandler(
+            Commands.LIGHTS_GROUP_OFF,
+            async (_id) => {
+                this.api.lightsOff(_id);
+            }
+        );
 
-
-
-    console.log(`[Companion] Initialized lights component`);
-  }
+        console.log(`[Companion] Initialized lights component`);
+    }
 }
